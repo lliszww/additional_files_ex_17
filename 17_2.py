54fbd551fc0ea269e2f6682ec3cdd10042643322
Elizaveta Nikulina <118754476+lliszww@users.noreply.github.com>
2024-07-17 02:54:08 +0300

    Update 17_2.py

--- 17_2.py
+++ 17_2.py
@@ -6,24 +6,18 @@ green = 0
 blue = 0
 
 colors = 0
-c = []
 
 for i in range(len(m) - 2):
     if (m[i] == 255) and (m[i + 1] == 0) and (m[i+2] == 0):
         colors += 1
         red += 1
-        c.append((m[i], m[i+1], m[i+2]))
+
     elif (m[i] == 0) and (m[i + 1] == 255) and (m[i+2] == 0):
         colors += 1
         green += 1
-        c.append((m[i], m[i + 1], m[i + 2]))
+
     elif (m[i] == 0) and (m[i + 1] == 0) and (m[i+2] == 255):
         colors += 1
         blue += 1
-        c.append((m[i], m[i + 1], m[i + 2]))
-
-print(c)
 
 print(colors, max(red, green, blue))
-
-
